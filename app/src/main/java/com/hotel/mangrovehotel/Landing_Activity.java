398f9012b2126b5a0dba7e669df616c07a29c97e
sajib_roy <sajibroy206@gmail.com>
2019-12-21 10:51:52 +0600

    sajib

--- app/src/main/java/com/hotel/mangrovehotel/Landing_Activity.java
+++ app/src/main/java/com/hotel/mangrovehotel/Landing_Activity.java
@@ -1,10 +1,14 @@
 package com.hotel.mangrovehotel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -13,7 +17,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Landing_Activity extends AppCompatActivity {
@@ -29,12 +35,124 @@ public class Landing_Activity extends AppCompatActivity {
 
     private ImageView cornercafe, mangobar, sevensession, hukalang, olivia, theoli;
 
+    private ImageView navimage;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    private ImageView lavespa;
+
+    private RelativeLayout layoutone, layouttwo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
 
+        drawerLayout = findViewById(R.id.LandingDrawerID);
+        navigationView = findViewById(R.id.LandingNavagationID);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
+                if (menuItem.getItemId() == R.id.LHomeID) {
+                    menuItem.setCheckable(true);
+                    menuItem.setChecked(true);
+                    drawerLayout.closeDrawer(Gravity.START);
+                }
+
+                if (menuItem.getItemId() == R.id.LMangroveCashID) {
+                    Toast.makeText(getApplicationContext(), "First complected login process", Toast.LENGTH_LONG).show();
+                    menuItem.setCheckable(true);
+                    menuItem.setChecked(true);
+                    drawerLayout.closeDrawer(Gravity.START);
+                }
+
+                if (menuItem.getItemId() == R.id.LFeedbackID) {
+                    menuItem.setCheckable(true);
+                    menuItem.setChecked(true);
+                    drawerLayout.closeDrawer(Gravity.START);
+                    Toast.makeText(getApplicationContext(), "First complected login process", Toast.LENGTH_LONG).show();
+
+                }
+
+                if (menuItem.getItemId() == R.id.LBestDealsID) {
+                    menuItem.setCheckable(true);
+                    menuItem.setChecked(true);
+                    drawerLayout.closeDrawer(Gravity.START);
+                    Intent intent = new Intent(getApplicationContext(), BestDeals.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+
+                if (menuItem.getItemId() == R.id.LHelpID) {
+                    menuItem.setCheckable(true);
+                    menuItem.setChecked(true);
+                    drawerLayout.closeDrawer(Gravity.START);
+                    Intent intent = new Intent(getApplicationContext(), HelpLineActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+
+                if (menuItem.getItemId() == R.id.LWhyMangroveID) {
+                    menuItem.setCheckable(true);
+                    menuItem.setChecked(true);
+                    drawerLayout.closeDrawer(Gravity.START);
+                    Intent intent = new Intent(getApplicationContext(), TramsAncConditions.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+
+                if (menuItem.getItemId() == R.id.LTramsConditionID) {
+                    menuItem.setCheckable(true);
+                    menuItem.setChecked(true);
+                    drawerLayout.closeDrawer(Gravity.START);
+                    Intent intent = new Intent(getApplicationContext(), TConditionActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+
+                if (menuItem.getItemId() == R.id.LReferFeiendID) {
+                    int counter = 0;
+                    menuItem.setCheckable(true);
+                    menuItem.setChecked(true);
+                    drawerLayout.closeDrawer(Gravity.START);
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.setType("text/plain");
+
+                    String shareMessage = "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID + "\n\n";
+
+                    String sharebody = shareMessage;
+                    String sharesubject = "Hi mangrove hotel provide beast services for you" + "\n\n" + sharebody + "\n";
+                    intent.putExtra(Intent.EXTRA_TEXT, sharesubject);
+                    //  intent.putExtra(Intent.EXTRA_SUBJECT, sharebody);
+                    startActivity(Intent.createChooser(intent, "share with"));
+                }
+
+                return true;
+            }
+        });
+
+
+        lavespa = findViewById(R.id.LaveSpaID);
+        lavespa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LaveSpaActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+
+        navimage = findViewById(R.id.NavImageID);
+        navimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startOpening_navagationmenu();
+            }
+        });
+
+/*
         offer = findViewById(R.id.PostButtonID);
         offer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +162,7 @@ public class Landing_Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+*/
 
         cornercafe = findViewById(R.id.CornerCafeID);
         cornercafe.setOnClickListener(new View.OnClickListener() {
@@ -55,8 +173,10 @@ public class Landing_Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        layoutone = findViewById(R.id.KKK);
         animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.frombutton);
         cornercafe.setAnimation(animation);
+        layoutone.setAnimation(animation);
 
 
         mangobar = findViewById(R.id.MangobarID);
@@ -118,6 +238,7 @@ public class Landing_Activity extends AppCompatActivity {
         });
 
         Mauth = FirebaseAuth.getInstance();
+     /*
         hotelweb = findViewById(R.id.HotelWebID);
         hotelweb.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -127,6 +248,7 @@ public class Landing_Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        */
 
         if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -136,12 +258,12 @@ public class Landing_Activity extends AppCompatActivity {
 
         joinbutton = findViewById(R.id.JoinButtonID);
         siginbutton = findViewById(R.id.SiginButtonID);
-
+        layouttwo = findViewById(R.id.Twos);
 
         animatable = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.frombutton);
         joinbutton.setAnimation(animatable);
         siginbutton.setAnimation(animatable);
-
+        layouttwo.setAnimation(animation);
 
 
         joinbutton.setOnClickListener(new View.OnClickListener() {
@@ -164,5 +286,11 @@ public class Landing_Activity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    private void startOpening_navagationmenu() {
+
+        drawerLayout.openDrawer(Gravity.START);
     }
 }
---
+++ app/src/main/java/com/hotel/mangrovehotel/LaveSpaActivity.java
@@ -0,0 +1,42 @@
+package com.hotel.mangrovehotel;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class LaveSpaActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TextView textView;
+    private Animation animation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lave_spa);
+
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.frombutton);
+        textView = findViewById(R.id.LaveSpaTextID);
+        textView.setAnimation(animation);
+
+        toolbar = findViewById(R.id.LaveSpaToolbarID);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_icon);
+        getSupportActionBar().setTitle("Lavie Spa");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
