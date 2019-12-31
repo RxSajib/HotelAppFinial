6077c26ba74da1becdb316a9ac30aed988700ceb
hasib74 <hasibakon74@gmail.com>
2019-12-31 12:48:14 +0600

    sssrwee

--- app/src/main/java/com/hotel/mangrovehotel/WelcomeActivity.java
+++ app/src/main/java/com/hotel/mangrovehotel/WelcomeActivity.java
@@ -27,7 +27,7 @@ public class WelcomeActivity extends AppCompatActivity {
             public void run() {
 
                 try {
-                    Thread.sleep(3000);
+                    Thread.sleep(0);
                     Intent intent = new Intent(getApplicationContext(), Landing_Activity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(intent);
